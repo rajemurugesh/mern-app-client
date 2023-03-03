6630b3113fd83964a4454267cfa909f4c0a073e6
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-03-03 10:38:52 +0530

    csss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
+    <footer className="fixed-bottom">
       <h6 className="align">
         © Copyright 2023 | Limat Tech Solutions. All rights reserved
       </h6>
+      </footer>
     </>
   );
 };
